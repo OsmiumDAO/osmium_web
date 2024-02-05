c001d785381ace504e97bd64bac988c0d566a5ed
= <=>
2024-02-05 04:07:09 +0100

    corrected wallets

--- src/data/wallets.js
+++ src/data/wallets.js
@@ -1,23 +1,23 @@
 const wallets = [
   {
-    title: 'Treasury Wallet',
+    title: "Treasury Wallet",
     address:
-      'addr1x84xk8ad06s03pd8yuxkrursufv4wzy3eld5zafhwxql6w3060czzmy4xl40lq2y0xt3eq93e726xv0tskpj0g52duzs8882nz',
+      "addr1x84xk8ad06s03pd8yuxkrursufv4wzy3eld5zafhwxql6w3060czzmy4xl40lq2y0xt3eq93e726xv0tskpj0g52duzs8882nz",
     // 'addr1x8pwxyr2zx5xpucxlns9ha4cjeqk902ewq3jmyn6wd7gsq7zuvgx5ydgvresdl8qt0mt39jpv274jupr9kf85umu3qps3hhrqr',
-    link: 'https://pool.pm/$Osmium',
+    link: "https://pool.pm/$osmium",
   },
   {
-    title: 'Royalty Wallet',
+    title: "Royalty Wallet",
     address:
-      'addr1q8ykctkdtzcx65vckmxmagjupa0vj4vam4v9y9vvk56d62jj22sujqj4kmdcd9ca63g265pprr76n30axqyz5ycvf7mqvcy9zz',
-    link: 'https://pool.pm/$Osgiveaway',
+      "addr1q8ykctkdtzcx65vckmxmagjupa0vj4vam4v9y9vvk56d62jj22sujqj4kmdcd9ca63g265pprr76n30axqyz5ycvf7mqvcy9zz",
+    link: "https://pool.pm/$osgiveaway",
   },
   {
-    title: 'Broker Wallet',
+    title: "Broker Wallet",
     address:
-      'addr1qytwphdz7lc4nam78jrahth8fea3a6etrj33z7kk0tmdr3r5w0dv5nsx9dtq9smmycp9jh5f5pyd8a04ynhfxh4cvrtszjzzg0',
-    link: 'https://pool.pm/addr1qytwphdz7lc4nam78jrahth8fea3a6etrj33z7kk0tmdr3r5w0dv5nsx9dtq9smmycp9jh5f5pyd8a04ynhfxh4cvrtszjzzg0',
+      "addr1qytwphdz7lc4nam78jrahth8fea3a6etrj33z7kk0tmdr3r5w0dv5nsx9dtq9smmycp9jh5f5pyd8a04ynhfxh4cvrtszjzzg0",
+    link: "https://pool.pm/addr1qytwphdz7lc4nam78jrahth8fea3a6etrj33z7kk0tmdr3r5w0dv5nsx9dtq9smmycp9jh5f5pyd8a04ynhfxh4cvrtszjzzg0",
   },
-]
+];
 
-export default wallets
+export default wallets;
